22293f8ef8cb7d09f83bc183b7b4c3cb028af68b
drsean3 <andyvelezr@gmail.com>
2017-09-12 11:52:56 -0500

    Regreso a origen
    Regreso a origen

--- service-single-mantprevent.php
+++ service-single-mantprevent.php
@@ -78,7 +78,14 @@
 <p>Limpieza y revisión de sistema de distribución de Aire. </p>
 <p>Revisión general del estado de corrosión del equipo, pintura en caso necesario. </p>
 <p>Cambio de piezas de recambio que por su obsolescencia técnica o desgaste sea necesaria sustituir. </p>
+<p>3. La frecuencia con que se realizaran estas operaciones de mantenimientos será de una intervención seis meses  como mínimo. Estableciéndose la frecuencia necesaria de estas intervenciones por un especialista en dependencia de lo agresivo que sea el ambiente donde se encuentren instalado los sistemas. </p>
+<p>4. <span>RCV. Air system</span>, realizará un análisis y evaluación periódica del rendimiento de los equipos generadores de frío en función de su potencia térmica nominal, midiendo y registrando los valores, de acuerdo con las operaciones y periodicidades. </p>
 
+</div>
+
+                            
+                            <p>Para evitar el deterioro de los equipos, es aconsejable realizar mantenimiento trimestralmente <br>Nosotros somos lideres y conocemos a fondo la mayor parte de dispositivos de aire acondicionado dando al cliente mayor confiabilidad a la hora de realizar manetenimientos.</p>
+                            <p>Tenemos alternativas y grandes descuentos si persiste el servicio de mantenimiento preventivo. Contactanos para mayor información a nuestras líneas de atención</p>
                         </div>
                     </div>       
                     <div class="col-md-6 col-sm-12">
@@ -141,15 +148,6 @@
                     </div>       
                 </div>
             </div>
-            <div>
-            <p>3. La frecuencia con que se realizaran estas operaciones de mantenimientos será de una intervención seis meses  como mínimo. Estableciéndose la frecuencia necesaria de estas intervenciones por un especialista en dependencia de lo agresivo que sea el ambiente donde se encuentren instalado los sistemas. </p>
-<p>4. <span>RCV. Air system</span>, realizará un análisis y evaluación periódica del rendimiento de los equipos generadores de frío en función de su potencia térmica nominal, midiendo y registrando los valores, de acuerdo con las operaciones y periodicidades. </p>
-</div>
-
-                            
-                            <p>Para evitar el deterioro de los equipos, es aconsejable realizar mantenimiento trimestralmente <br>Nosotros somos lideres y conocemos a fondo la mayor parte de dispositivos de aire acondicionado dando al cliente mayor confiabilidad a la hora de realizar manetenimientos.</p>
-                            <p>Tenemos alternativas y grandes descuentos si persiste el servicio de mantenimiento preventivo. Contactanos para mayor información a nuestras líneas de atención</p>
-            <div>
         </section>
         <!--/single-project-->
 		
